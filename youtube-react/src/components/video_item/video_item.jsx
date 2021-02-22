818d5546d3b6d7d294d7aa19806310b164df1996
유진우 <phoenix9373@naver.com>
2021-02-22 23:39:16 +0900

    [add] apply flex wrap

--- youtube-react/src/components/video_item/video_item.jsx
+++ youtube-react/src/components/video_item/video_item.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+// 극한의 destructuring
+const VideoItem = ({ video: { snippet: info } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={info.thumbnails.medium.url}
+        alt="thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{info.title}</p>
+        <p className={styles.channel}>{info.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
 export default VideoItem;
